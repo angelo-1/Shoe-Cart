b3052e6599efc037503a035f922e78764260da9c
Angelo Rajesh <angelo11@gmail.com>
2023-05-31 12:12:16 +0530

    1st Commit

---
+++ src/pages/shop/Shop.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {PRODUCTS} from "../../products"
+import { Product } from './Product'
+import './shop.css'
+
+function Shop() {
+  return (
+    <>
+    <div className='shop'>
+        <div className='shopTitle'>
+            <h1>Shoe Mart</h1>
+        </div>
+        <div className='products'>
+          {PRODUCTS.map((product)=> <Product data={product} />)}
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Shop
---
+++ src/products.js
@@ -0,0 +1,40 @@
+import product1 from './assets/shoe1.jpg'
+import product2 from './assets/shoe2.png'
+import product3 from './assets/shoe3.png'
+import product4 from './assets/shoe4.jpg'
+import product5 from './assets/shoe5.png'
+
+export const PRODUCTS = [
+    {
+        id:1,
+        productName:"product1",
+        price: 999.0,
+        productImage: product1,
+    },
+    {
+        id:2,
+        productName:"product2",
+        price: 1200.0,
+        productImage: product2,
+    },
+    {
+        id:3,
+        productName:"product3",
+        price: 1100.0,
+        productImage: product3,
+    },
+    {
+        id:4,
+        productName:"product4",
+        price: 1200.0,
+        productImage: product4,
+    },
+    {
+        id:5,
+        productName:"product5",
+        price: 1400.0,
+        productImage: product5,
+    },
+
+
+];
